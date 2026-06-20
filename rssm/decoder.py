c66dbe7040fb5071bffec035a6c55473c072ee4f
paul2pam <paul2pam@yahoo.com>
2026-06-20 16:12:46 -0700

    Decoder working

--- rssm/decoder.py
+++ rssm/decoder.py
@@ -2,26 +2,26 @@ import torch.nn as nn
 import torch
 
 class Decoder(nn.Module):
-    def __init__(self, batch_size, deter_dim, discrete_dim):
+    def __init__(self, deter_dim, discrete_dim):
         super().__init__()
-        self.batch_size = batch_size
         self.total_dim = deter_dim + discrete_dim
         self.stride = 2
 
-        self.convs == nn.Sequential(
-            nn.Linear(in_features = self.embed_dim, out_features = 4096),
+        self.convs = nn.Sequential(
+            nn.Linear(in_features = self.total_dim, out_features = 4096),
             nn.Unflatten(-1, (256,4,4)),
             nn.SiLU(),
-            nn.Conv2d(256, 128, 3, stride=self.stride, padding=1),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=self.stride, padding=1),
             nn.SiLU(),
-            nn.Conv2d(128, 64, 3, stride = self.stride, padding=1),
+            nn.ConvTranspose2d(128, 64, 4, stride = self.stride, padding=1),
             nn.SiLU(),
-            nn.Conv2d(64,32,3, stride=self.stride, padding=1),
+            nn.ConvTranspose2d(64, 32, 4, stride=self.stride, padding=1),
             nn.SiLU(),
-            nn.Conv2d(32, 3, 3, stride = self.stride, padding=1)
+            nn.ConvTranspose2d(32, 3, 4, stride = self.stride, padding=1)
         )
 
 
     def forward(self, h, z):   
         input = torch.cat((h,z), dim=-1)
         x = self.convs(input)
+        return x
